e5da39680fe5bdad3a6c162c248efee19083c105
Niall Eccles <nialle@rothco.ie>
2018-06-01 17:09:38 +0100

    Added in some new food items. Added in animations for page load.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './ui/nav/nav.component';
@@ -29,6 +30,7 @@ const appRoutes: Routes = [
       { enableTracing: false }
     ),
     HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/view/menu/full/full.component.ts
+++ src/app/view/menu/full/full.component.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { JsonService } from '../../../services/json.service';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-full',
   templateUrl: './full.component.html',
-  styleUrls: ['./full.component.css']
+  styleUrls: ['./full.component.css'],
+  animations: [
+    trigger('goals',[
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ])
+        )]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class FullComponent implements OnInit {
   constructor(private http: HttpClient, private jsonService: JsonService) {}
+
+  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
 
   public fooddata:any;
 
