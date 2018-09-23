1329e04199b1f06e01f50acab4deb37b06635865
Niall Eccles <n.eccles14@gmail.com>
2018-09-23 21:02:59 +0100

    Added in environment variables.

---
+++ src/environments/variables.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDmzZtYxioasWQ1kE0nwTaWjSGhAiz8Uys',
+    authDomain: 'roast-ee767.firebaseapp.com',
+    databaseURL: 'https://roast-ee767.firebaseio.com',
+    projectId: 'roast-ee767',
+    storageBucket: 'roast-ee767.appspot.com',
+    messagingSenderId: '441470841187'
+  }
+};
